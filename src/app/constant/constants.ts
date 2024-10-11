b3c7006dd30e177a268f22ab48faa8762f137c4a
sanketparsewar <sanket.parsewar97@gmail.com>
2024-10-11 15:34:40 +0530

    added constant file to store api url

---
+++ src/app/constant/constants.ts
@@ -0,0 +1,9 @@
+export const Constant = {
+  API_URL: 'https://projectapi.gerasim.in/api/Complaint/',
+  DEPARTMENT_METHOD: {
+    GET_ALL: 'GetParentDepartment',
+    POST_NEW: 'AddnewDepartment',
+    PUT_UPDATE: 'updateDepartment',
+    DELETE_BY_ID: 'DeletedepartmentBydepartmentId',
+  },
+};
--- src/app/service/department.service.ts
+++ src/app/service/department.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Constant } from '../constant/constants';
 
 @Injectable({
   providedIn: 'root',
@@ -8,20 +9,27 @@ export class DepartmentService {
   http = inject(HttpClient);
   constructor() {}
 
-  apiURL: string = 'https://projectapi.gerasim.in/api/Complaint/';
+  // apiURL: string = 'https://projectapi.gerasim.in/api/Complaint/';
 
   getApi() {
-    return this.http.get(this.apiURL + 'GetParentDepartment');
+    return this.http.get(Constant.API_URL + Constant.DEPARTMENT_METHOD.GET_ALL);
   }
   postApi(obj: any) {
-    return this.http.post(`${this.apiURL}AddnewDepartment`, obj);
+    return this.http.post(
+      `${Constant.API_URL}${Constant.DEPARTMENT_METHOD.POST_NEW}`,
+      obj
+    );
   }
   putApi(obj: any) {
-    return this.http.post(this.apiURL + 'updateDepartment', obj);
+    return this.http.post(
+      Constant.API_URL + Constant.DEPARTMENT_METHOD.PUT_UPDATE,
+      obj
+    );
   }
   deleteApi(depId: number) {
     return this.http.delete(
-      `${this.apiURL}DeletedepartmentBydepartmentId?departmentId=` + depId
+      `${Constant.API_URL}${Constant.DEPARTMENT_METHOD.DELETE_BY_ID}?departmentId=` +
+        depId
     );
   }
 }
